b30b54df37640870a84a8d6cc0f724b36060d8d2
joseproig <josep.roig@students.salle.url.edu>
2019-04-16 22:39:33 +0200

    feat: Creados los models necesarios para el programa

---
+++ src/Model/Post.java
@@ -0,0 +1,16 @@
+package Model;
+
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+    private int id;
+    private List<String> liked_by;
+    private long published_when;
+    private String published_by;
+    private List<Double> location;
+    private List<String> hashtags;
+
+}
---
+++ src/Model/User.java
@@ -0,0 +1,13 @@
+package Model;
+
+import com.sun.org.apache.xerces.internal.impl.dtd.models.DFAContentModel;
+
+import java.util.List;
+
+public class User {
+    //Atributs de la classe User
+    private String username;
+    private long creation;
+    private List<String> to_follow;
+
+}
