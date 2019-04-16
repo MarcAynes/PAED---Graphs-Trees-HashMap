5e7888a296c1649b5187669246fb2f27440c6c0d
MarcAynes <marcaynes2@gmail.com>
2019-04-16 19:39:53 +0200

    feat comprobacio de la insercio

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println();
+        Node node = new Node(40);
+        ArbreAVL arbre = new ArbreAVL(node);
+
+        for (int i = 0; i < 10; i++){
+            Random rn = new Random();
+            Node aux = new Node(rn.nextInt(100));
+            arbre.add(aux);
+        }
+        System.out.println("aiudame");
 
     }
 }
