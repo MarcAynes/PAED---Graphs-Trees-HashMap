39ff8a6bb0141e682fd52ca622dd1053a87dfcf4
joseproig <josep.roig@students.salle.url.edu>
2019-04-17 21:53:37 +0200

    feat: Creados los atributos de las clases basicas

--- src/RTree/RTree.java
+++ src/RTree/RTree.java
@@ -1,5 +1,12 @@
 package RTree;
 
 public class RTree {
-
+    private int min;
+    private int max;
+    private byte tipo; //Para saber si en el principio hay puntos o rectangulos
+    private int altura;
+    private Object [] raiz;
+    private int cantidad;
 }
+
+
--- src/RTree/Rectangulo.java
+++ src/RTree/Rectangulo.java
@@ -1,4 +1,10 @@
 package RTree;
 
 public class Rectangulo {
+    private Nodo hijo;
+    private double xMax;
+    private double xMin;
+    private double yMax;
+    private double yMin;
+
 }
