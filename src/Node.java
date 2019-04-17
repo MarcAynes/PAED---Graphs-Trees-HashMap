b0eb8e51fa3f51a9838f22d1dab29228eba071f7
MarcAynes <marcaynes2@gmail.com>
2019-04-17 19:26:39 +0200

    feat: petit error al inserir
    resolt error al inserir

--- src/Node.java
+++ src/Node.java
@@ -30,7 +30,7 @@ public class Node {
 
     public void add(Node node){
 
-        if (node.getNumero() < numero) { //ex: numero actual 5 numero a inserir el 7
+        if (node.getNumero() > numero) { //ex: numero actual 5 numero a inserir el 7
                                         // 7 al fill dret
             if (filldret != null) {
 
